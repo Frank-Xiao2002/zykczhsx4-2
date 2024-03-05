bbe4149565da0e9c3982b4c373d2c96051274c33
Frank-Xiao2002 <Xiao670127@126.com>
2024-03-05 15:23:47 +0800

    init commit

---
+++ src/main/java/com/xxj/zykczhsx42/Zykczhsx42Application.java
@@ -0,0 +1,13 @@
+package com.xxj.zykczhsx42;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zykczhsx42Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zykczhsx42Application.class, args);
+    }
+
+}
